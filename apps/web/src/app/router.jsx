b6ef5e95f532779f53af80eed236440ced2a2d26
Bob Morganstern <bobmorganstern@gmail.com>
2026-07-08 01:39:36 -0700

    feat: add application route map

---
+++ apps/web/src/app/router.jsx
@@ -0,0 +1,6 @@
+export const routes = [
+  { path: '/', name: 'Dashboard' },
+  { path: '/interviews', name: 'Interviews' },
+  { path: '/analysis/:id', name: 'Analysis' },
+  { path: '/video/:id', name: 'Interview Studio' }
+];
